5ac61acd6380c7c1a2ec1a1e8e814b9d0225f91c
Luke Miller <millerlp@gmail.com>
2015-05-05 18:16:59 -0700

    Just a simple blink sketch to show that the bootloader works and allows serial uploading.

---
+++ Test_sketches/Simple_blink/Simple_blink.ino
@@ -0,0 +1,27 @@
+/*
+	Tested and functional on MusselTracker board v1
+	uploaded via FTDI usb serial chip after burning a
+	copy of ATmegaBOOT_168_atmega328_pro_8MHz.hex to the
+	chip using the profile laid out in my boards.txt entry
+	"Mussel Tracker2 (328P @ 8 MHz external, 3.3V ATmegaBOOT)"
+	
+	LPM 2015-05-05
+
+*/
+
+const int heartbeatLED = 8;
+const int errorLED = 9;
+
+void setup(void){
+	Serial.begin(57600);
+	Serial.println("Hello world");
+	pinMode(heartbeatLED, OUTPUT);
+	pinMode(errorLED, OUTPUT);
+	digitalWrite(heartbeatLED, HIGH);
+}
+
+void loop(void) {
+	digitalWrite(heartbeatLED, !digitalRead(heartbeatLED));
+	digitalWrite(errorLED, !digitalRead(errorLED));
+	delay(500);
+}
