3b519976c4b8204c3b156849b3f6833ff889e384
Luke Miller <millerlp@gmail.com>
2015-06-29 14:04:39 -0700

    First commit, nothing to see yet

---
+++ Test_sketches/TC_calibrate/TC_calibrate.ino
@@ -0,0 +1,25 @@
+/* TC_calibrate.ino
+
+	Use this sketch with the Mussel Tracker v2 boards
+	to record thermocouple temperatures to the SD 
+	card. 
+
+
+*/
+
+#include "SdFat.h" // https://github.com/greiman/SdFat
+#include <Wire.h>
+#include <SPI.h>
+#include "RTClib.h" // https://github.com/millerlp/RTClib
+#include "Adafruit_MAX31855.h" // https://github.com/adafruit/Adafruit-MAX31855-library
+#include "MusselTrackerlib.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
